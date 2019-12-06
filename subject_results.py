d40940c71942998812518cf7509545682787cbdd
Lindsay Ward <lindsay.ward@jcu.edu.au>
2019-12-06 18:03:07 +1000

    Add function to write students to results output data sheet

--- subject_results.py
+++ subject_results.py
@@ -4,12 +4,12 @@ Create subject results comprehensive spreadsheet from:
 - LearnJCU Ultra grade centre download/export
 """
 import csv
-import xlrd
+import openpyxl
 
 DIRECTORY_DATA = 'data/subject_results'
 FILE_CLASS_LIST = 'class_list_from_sms.csv'
-FILE_GRADES = 'learnjcu_grade_centre.xls'
-FILE_OUTPUT = 'blank_results.xlsx'
+FILE_GRADE_CENTRE = 'learnjcu_grade_centre.xls'
+FILE_RESULTS = 'blank_results.xlsx'
 
 
 def main():
@@ -19,22 +19,32 @@ def main():
     print(f"Got {len(students)} students from {FILE_CLASS_LIST}")
     # print(students)
 
+    add_students_to_results(students)
+
 
 def get_students():
     """Get students as a list of tuples from the class list."""
-    students = []  # list of tuples
+    students = []  # list of rows/lists
     input_file = open(f"{DIRECTORY_DATA}/{FILE_CLASS_LIST}", 'r')
     input_file.readline()  # Ignore first header line
-    fields = input_file.readline().split(',')  # Get field names
-    index_id = fields.index('Student Id')
-    index_name = fields.index('Student Name')
-    # print(index_id, index_name)
+    input_file.readline()  # Ignore second header line
     reader = csv.reader(input_file)
     for row in reader:
-        student = row[index_id], row[index_name]
+        student = row
         students.append(student)
     input_file.close()
     return students
+
+
+def add_students_to_results(students):
+    """Add students to StudentOne data sheet."""
+    workbook = openpyxl.load_workbook(filename=f"{DIRECTORY_DATA}/{FILE_RESULTS}")
+    sheet = workbook['StudentOne']
+    first_row = 3
+    for i, student in enumerate(students):
+        for j, value in enumerate(student, 1):
+            sheet.cell(row=first_row + i, column=j, value=value)
+    workbook.save(filename='test_output.xlsx')
 
 
 main()
