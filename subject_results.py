b129d4a7efad69397489515fa06106644e0b250b
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-06-24 17:30:36 +1000

    Add assessment weight and CSV output

--- subject_results.py
+++ subject_results.py
@@ -8,43 +8,54 @@ TODO: Add error-checking for grade centre sheet (<= 5 assessment items)
 """
 import csv
 import openpyxl
+import warnings
+import xlwings
 
 # Constants ordered/grouped by relevant spreadsheet
-DIRECTORY_DATA = 'data/subject_results'
-FILE_RESULTS = 'blank_results.xlsx'
 
-FILE_CLASS_LIST = 'class_list_from_sms.csv'
-COLUMN_CLASS_LIST_ID = 13  # N
-
+DIRECTORY_DATA = 'data/subject_results/covid'
+FILE_RESULTS_BLANK = 'CP-2020-1-T-Results.xlsx'
 FILE_GRADE_CENTRE = 'learnjcu_grade_centre.xls'
-LAST_HEADING_BEFORE_ASSESSMENTS = 'Child Subject ID'
+FILE_IN_CLASS_LIST = 'CP3402 Cairns SP1 2020.csv'
+# FILE_CLASS_LIST = 'class_list_from_sms.csv'
+FILE_OUTPUT = "results.xlsx"  # TODO - use IN_CLASS name + Results
+
+
 SHEET_STUDENT = 'StudentOne'
 SHEET_RESULTS = 'RawResults'
+COLUMN_CLASS_LIST_ID = 13  # N
+LAST_HEADING_BEFORE_ASSESSMENTS = 'Child Subject ID'  # for the column number without using an absolute number
 ROW_FIRST_STUDENT_ONE = 3
 ROW_FIRST_RAW_DATA = 13
-COLUMN_GRADE_CENTRE_ID = 3
-COLUMN_GRADE_CENTRE_FIRST_ASSESSMENT = 5
+COLUMN_GRADE_CENTRE_ID = 3  # Numbered from 1 (csv)
+COLUMN_RESULTS_FIRST_ASSESSMENT = 5  # Numbered from 0 (openpyxl)
 
 
 def main():
     """Create subject results comprehensive spreadsheet."""
-    print("Welcome")
+    warnings.filterwarnings("ignore")
     students_class_list = get_students()
-    print(f"Got {len(students_class_list)} students from {FILE_CLASS_LIST}")
+    print(f"Got {len(students_class_list)} students from {FILE_IN_CLASS_LIST}")
 
     # add_students_to_results(students_class_list)
     assessments, student_grade_centre_rows = get_assessments()
+    print(f"Got {len(assessments)} assessments from {FILE_GRADE_CENTRE}")
+
     # assessments = [(7, 'Assignment 1 - Movies to Watch 1.0', 100.0), (8, 'Assignment 2 - Movies to Watch 2.0', 100.0), (9, 'Pracs', 30.0)]
     student_results = get_student_results(student_grade_centre_rows, students_class_list, assessments)
     if len(students_class_list) != len(student_results):
         print("ERROR: Student results don't match class list")
+        return  # TODO: raise exception
+    print(f"Got {len(student_results)} students' results from {FILE_GRADE_CENTRE}")
     write_results(student_results, students_class_list, assessments)
+    print(f"Results spreadsheet created: {DIRECTORY_DATA}/{FILE_OUTPUT}")
+    write_csv()
 
 
 def get_students():
     """Get students as a list of tuples from the class list."""
     students = []  # list of rows/lists
-    input_file = open(f"{DIRECTORY_DATA}/{FILE_CLASS_LIST}", 'r')
+    input_file = open(f"{DIRECTORY_DATA}/{FILE_IN_CLASS_LIST}", 'r')
     input_file.readline()  # Ignore first header line
     input_file.readline()  # Ignore second header line
     reader = csv.reader(input_file)
@@ -67,15 +78,21 @@ def get_assessments():
     first_assessment_index = headings.index(LAST_HEADING_BEFORE_ASSESSMENTS) + 1
     assessment_headings = headings[first_assessment_index:]
     assessments = []
-    # Assessment names look like:
-    # heading = "Assignment 1 - Movies to Watch 1.0 [Total Pts: 100 Score] |660463"
-    # heading = "Pracs [Total Pts: up to 30 Score] |789219"
+    # Assessment data looks like (last value set manually in spreadsheet):
+    # heading = "Assignment 1 - Movies to Watch 1.0 [Total Pts: 100 Score] |20"
+    # heading = "Pracs [Total Pts: up to 30 Score] |10"
     for i, heading in enumerate(assessment_headings):
-        heading = heading.replace("up to ", "")  # for total columns
-        parts = heading.split(" [Total Pts: ")
-        title = parts[0]
-        score = float(parts[1].split()[0])
-        assessments.append((first_assessment_index + i, title, score))
+        try:
+            heading = heading.replace("up to ", "")  # for total columns
+            parts = heading.split(" [Total Pts: ")
+            title = parts[0]
+            sub_parts = parts[1].split()
+            score = float(sub_parts[0])
+            weight = int(sub_parts[-1].strip('|'))
+            assessments.append((first_assessment_index + i, title, score, weight))
+        except IndexError:
+            print(f"ERROR with format of {DIRECTORY_DATA}/{FILE_GRADE_CENTRE}")
+            return None  # TODO raise exception
     # Return both processed assessments and rest of student data rows
     return assessments, rows[1:]
 
@@ -94,6 +111,7 @@ def get_student_results(student_grade_centre_rows, students, assessments):
             print(f"ERROR: Student: {student_name} ({student_id}) not in the LearnJCU Grade Centre sheet")
             continue
         for assessment in assessments:
+            # Handle things like "Reply to Post(81.60)" in LearnJCU with total column
             # Replace non-scores like 'In Progress' or 'Needs Grading' with blanks
             try:
                 score = float(student_grade_centre_rows[index][assessment[0]])
@@ -106,7 +124,7 @@ def get_student_results(student_grade_centre_rows, students, assessments):
 
 def write_results(student_results, class_list, assessments):
     """Write student details and scores to results spreadsheet."""
-    workbook = openpyxl.load_workbook(filename=f"{DIRECTORY_DATA}/{FILE_RESULTS}")
+    workbook = openpyxl.load_workbook(filename=f"{DIRECTORY_DATA}/{FILE_RESULTS_BLANK}")
     # Add student scores to results data sheet
     sheet = workbook[SHEET_STUDENT]
     # Write all data from class list to results StudentOne sheet
@@ -127,11 +145,13 @@ def write_results(student_results, class_list, assessments):
         sheet.cell(row=current_row, column=3, value=f"={SHEET_STUDENT}!O{reference_row}")
 
     # Add assessment headings (E10-J12, 10=%, 11=Out Of, 12=Description)
-    # assessment looks like (7, 'Assignment 1 - Movies to Watch 1.0', 100.0)
+    # assessment looks like (7, 'Assignment 1 - Movies to Watch 1.0', 100.0, 20)
     for i, assessment in enumerate(assessments):
-        # Unsure if possible to get %. Could get staff to enter it in Grade Centre
-        sheet.cell(row=11, column=COLUMN_GRADE_CENTRE_FIRST_ASSESSMENT + i, value=assessment[2])
-        sheet.cell(row=12, column=COLUMN_GRADE_CENTRE_FIRST_ASSESSMENT + i, value=assessment[1])
+        # Note: staff need to enter weight in Grade Centre
+        weight_to_write = assessment[3] / 100  # Need 0-1 instead of 0-100 for % in spreadsheet
+        sheet.cell(row=10, column=COLUMN_RESULTS_FIRST_ASSESSMENT + i, value=weight_to_write)
+        sheet.cell(row=11, column=COLUMN_RESULTS_FIRST_ASSESSMENT + i, value=assessment[2])
+        sheet.cell(row=12, column=COLUMN_RESULTS_FIRST_ASSESSMENT + i, value=assessment[1])
 
     # Add scores
     for i, current_student_results in enumerate(student_results):
@@ -139,9 +159,27 @@ def write_results(student_results, class_list, assessments):
         for j, score in enumerate(current_student_results[2:]):
             if score is None:  # Don't write blanks
                 continue
-            current_column = COLUMN_GRADE_CENTRE_FIRST_ASSESSMENT + j
+            current_column = COLUMN_RESULTS_FIRST_ASSESSMENT + j
             sheet.cell(row=current_row, column=current_column, value=score)
-    workbook.save(filename='test_output.xlsx')  # TODO: temporary. Save actual file.
+    workbook.save(filename=f"{DIRECTORY_DATA}/{FILE_OUTPUT}")
+
+
+def write_csv(filename=""):
+    """Write just CSV file needed for results submission."""
+    if not filename:
+        filename = FILE_OUTPUT.replace(FILE_OUTPUT.split('.')[-1], "csv")
+
+    # openpyxl does not evaluate the formula, so use xlwings
+    excel_app = xlwings.App(visible=False)
+    excel_book = excel_app.books.open(f"{DIRECTORY_DATA}/{FILE_OUTPUT}")
+    sheet = excel_book.sheets[SHEET_STUDENT]
+    with open(f"{DIRECTORY_DATA}/{filename}", 'w', newline="") as f:
+        writer = csv.writer(f)
+        for row in sheet.range('A1').current_region.value:
+            writer.writerow(row)
+    excel_book.save()
+    excel_book.close()
+    excel_app.quit()
 
 
 main()
