4a89f4ac81a064ec669ce1929854df1969b7d874
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-11-02 17:11:24 +1000

    Complete get_assessments program including writing to spreadsheet

--- get_assessments.py
+++ get_assessments.py
@@ -2,6 +2,7 @@
 
 import requests
 import openpyxl
+from collections import OrderedDict
 
 YEAR = 2020
 START_STRING = "<h3>Subject Assessment</h3>"
@@ -28,29 +29,25 @@ HTML_BOTTOM = """
 </body>
 </html>
 """
-WILL_WRITE_HTML = False
 
 
 def main():
-    subjects = ['CP1401', 'CP1402']
-    # subjects = get_subjects()
+    all_subject_details = [OrderedDict(), OrderedDict()]  # assuming only 2 years
+    # subjects = ['CP1401', 'CP1402']
+    subjects = get_subjects()
     print(subjects)
 
-    if WILL_WRITE_HTML:
-        file_out = open(OUTPUT_HTML_FILENAME, 'w')
-        print(HTML_TOP, file=file_out)
+    file_out = open(OUTPUT_HTML_FILENAME, 'w')
+    print(HTML_TOP, file=file_out)
     subject_to_items = {}
 
-    if WILL_WRITE_HTML:
-        print(f"<table>", file=file_out)
+    print(f"<table>", file=file_out)
     for subject in subjects:
         # Current year
-        if WILL_WRITE_HTML:
-            print(f"<tr>", file=file_out)
+        print(f"<tr>", file=file_out)
         for i in range(2):
             year_to_get = YEAR + i
-            if WILL_WRITE_HTML:
-                print(f"<td><h2>{subject} - {year_to_get}</h2>", file=file_out)
+            print(f"<td><h2>{subject} - {year_to_get}</h2>", file=file_out)
             url = f"https://secure.jcu.edu.au/app/studyfinder/index.cfm?subject={subject}&year={year_to_get}&transform=subjectwebview.xslt"
             response = requests.get(url)
             text = response.text
@@ -58,17 +55,13 @@ def main():
             print(assessment_block, file=file_out)
             items = extract_items(assessment_block)
             subject_to_items[subject] = items
-            # print(subject_to_items)
-            if WILL_WRITE_HTML:
-                print(f"</td>", file=file_out)
-        if WILL_WRITE_HTML:
-            print(f"</tr>", file=file_out)
-    if WILL_WRITE_HTML:
-        print(f"</table>", file=file_out)
-
-    if WILL_WRITE_HTML:
-        print(HTML_BOTTOM, file=file_out)
-        file_out.close()
+            all_subject_details[i][subject] = items
+            print(f"</td>", file=file_out)
+        print(f"</tr>", file=file_out)
+    print(f"</table>", file=file_out)
+    print(HTML_BOTTOM, file=file_out)
+    file_out.close()
+    write_spreadsheet(all_subject_details)
 
 
 def get_subjects():
@@ -93,20 +86,34 @@ def extract_items(block):
     raw_items = [part.strip().strip('<li>').strip('</li>') for part in parts if part.strip().startswith('<li>')]
     for raw_item in raw_items:
         parts = raw_item.split(' - ')
-        assessment = parts[0]
+        assessment = parts[0].replace('&gt;', '>')
         weight = parts[1].strip('(').strip('%)')
         items.append((assessment, weight))
     return items
 
 
-def write_spreadsheet():
+def write_spreadsheet(all_subject_details):
+    # all_subject_details contains 2 dictionaries
     workbook = openpyxl.load_workbook(filename=OUTPUT_EXCEL_FILENAME)
     sheet = workbook['Assessment-Mapping']
-
-    sheet.cell(row=12, column=1, value='Hello')
+    row = 12  # first row for assessment items
+    column = 2
+    for i in range(2):  # for both years/dictionaries
+        for subject, items in all_subject_details[i].items():
+            # sheet.cell(row=row, column=column, value=subject)
+            item_row = row
+            for item_number, item in enumerate(items):
+                name, weight = item
+                try:
+                    weight = int(weight)
+                except ValueError:
+                    pass
+                sheet.cell(row=item_row + item_number, column=column, value=name)
+                sheet.cell(row=item_row + item_number, column=column + 3, value=weight)
+            column += 4  # distance to next subject (4 pieces of data per assessment)
+        row += 7  # move down to write next year's items
+        column = 2
     workbook.save(filename=f"output/temp.xlsx")
 
-
-# write_spreadsheet()
 
 main()
