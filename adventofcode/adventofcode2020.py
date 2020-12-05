c768ee5719b009a26378e8228519635c2133af7c
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-12-05 14:17:51 +1000

    Add Advent of Code day 3b

--- adventofcode/adventofcode2020.py
+++ adventofcode/adventofcode2020.py
@@ -2,6 +2,7 @@
 Advent of Code 2020
 https://adventofcode.com/2020/
 """
+import numpy
 
 
 def day_1():
@@ -37,23 +38,31 @@ def day_2():
 
 
 def day_3():
-    slope = (3, 1)  # right, down
-    number_of_trees = 0
-    position = 0
+    slopes = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))  # right, down
+    results = []
     file_in = open('day3.txt')
-    _ = file_in.readline()  # skip first line
-    for line in file_in:
-        elements = list(line.strip())
-        position += slope[0]
-        while True:
-            try:
-                if elements[position] == '#':
-                    number_of_trees += 1
-                break  # successful; no need to extend pattern
-            except IndexError:
-                elements += elements
+    lines = file_in.readlines()[1:]  # skip first line
     file_in.close()
-    print(number_of_trees)
+    lines = [list(line.strip()) for line in lines]
+
+    for slope in slopes:
+        number_of_trees = 0
+        position = 0
+        for i, line in enumerate(lines):
+            if slope[1] == 2 and i % 2 == 0:  # not extensible, but avoid preoptimising :)
+                continue  # skip a line due to slope of 2
+            position += slope[0]
+            while True:
+                try:
+                    if line[position] == '#':
+                        number_of_trees += 1
+                    break  # successful; no need to extend pattern
+                except IndexError:
+                    line += line
+        # print(number_of_trees)
+        results.append(number_of_trees)
+    print(results)
+    print(numpy.prod(results))
 
 
 day_3()
